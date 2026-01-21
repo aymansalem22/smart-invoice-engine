1d34c961e18bacefadcfd88af5608877db71fee6
your.account@bbva.com <ayman.abusafia.abusafia.tech@bbva.com>
2026-01-21 16:40:13 +0100

    Initial commit

---
+++ src/main/java/com/paltecno/fintech/invoicing/SmartInvoiceEngineApplication.java
@@ -0,0 +1,13 @@
+package com.paltecno.fintech.invoicing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartInvoiceEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartInvoiceEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paltecno/fintech/invoicing/SmartInvoiceEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paltecno.fintech.invoicing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartInvoiceEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
